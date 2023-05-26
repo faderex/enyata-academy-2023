8c650dd7dbc3e5f1092542a7c922a37f5ec1d72c
faderex <josephlarbi264@gmail.com>
2023-05-26 02:02:57 +0000

    assignment 3

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,25 @@
+//Answer 1
+let myName = "Joseph";
+
+// Answer 2
+console.log(typeof myName);
+
+// Answer 3
+myName = "Larbi";
+
+// Answer 4
+console.log(myName);
+
+// Assignment 5
+let car = {
+  carName: "Chevrolet Silverado",
+  yearOfManufacture: 2024,
+  engineType: "V8 Gas Engine",
+  exteriorColor: "Summit white",
+  interiorColor: "Jet Balck",
+  condition: "New",
+  transmission: "Automatic",
+  modelCode: "CK20743",
+  vin: "2GC4YME75R1101396",
+  bodyStyle: "Crew Cab",
+};
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,17 @@
+// Answer 1
+const favoriteNumber = 6;
+
+// Answer 2
+const halfOfFavNum = favoriteNumber / 2;
+
+// Answer 3
+let isaacsFavorite = 13;
+
+// Answer 4
+const difference = isaacsFavorite - favoriteNumber;
+
+// Answer 5
+isaacsFavorite = isaacsFavorite * 26;
+
+// Answer 6
+isaacsFavorite %= 3;
